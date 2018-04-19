435a4649bd1631a215c1a9d26b8eb9d51c761db2
yesheng <dasheng523@163.com>
2018-04-19 22:15:56 +0800

    sohu

--- src/clj/toutiao2/zawu/niushida.clj
+++ src/clj/toutiao2/zawu/niushida.clj
@@ -2,10 +2,17 @@
   (:require [toutiao2.driver :as tdriver]
             [etaoin.api :refer :all]
             [etaoin.keys :as ek]
+            [clojure.core.async :as async]
             [cheshire.core :as json]
             [clojure.java.io :as io]
             [clojure.string :as str]
             [toutiao2.config :refer [isWindows?]]))
+
+
+(def content-chan (async/chan))
+
+(defn handle-content [content]
+  (async/put! content-chan content))
 
 (defn- search-driver []
   (firefox
@@ -33,43 +40,51 @@
 
 (defn souhu-search [driver searchkey]
   (go driver (str "http://search.sohu.com/?keyword=" searchkey))
-  (for [i (range 20)]
-    (let [detail-node {:css (str ".ImageNewsCardContent:nth-child(" i ") > a")}]
-      (when (exists? driver detail-node)
+  (loop [i 1]
+    (let [detail-node [{:tag :div :class "ArticleFeed"}
+                       {:tag :div :index i}
+                       {:tag :div :class "ant-card-body"}
+                       {:tag :a}]]
+      (when (and (exists? driver detail-node)
+                 (< i 20))
         (scroll-query driver detail-node)
         (click driver detail-node)
+        (wait 1)
         (switch-window driver (last (get-window-handles driver)))
-        (let [content (souhu-content driver)]
-          (switch-window driver (first (get-window-handles driver)))
-          content)))))
+        (wait 5)
+        (handle-content (souhu-content driver))
+        (close-window driver)
+        (switch-window driver (first (get-window-handles driver)))
+        (recur (+ i 1))))))
+
 
 (defn baidu-search [driver searchkey]
   (go driver "https://www.baidu.com")
   (fill driver {:css "input.s_ipt"} searchkey ek/enter))
 
-(def driver (search-driver))
-(baidu-search driver "奔驰")
-
-(defn baidu-current-page [index]
-  (let [node [{:tag :div :class "result c-container " :index index} {:tag :h3} {:tag :a}]]
+(defn baidu-current-page [driver index]
+  (let [node [{:tag :div :class "result c-container " :index index}
+              {:tag :h3}
+              {:tag :a}]]
     (when (exists? driver node)
+      (scroll-query driver node)
       (click driver node)
+      (wait 1)
       (switch-window driver (last (get-window-handles driver)))
-      (let [content (get-element-text driver {:css ".article"})]
-        (switch-window driver (first (get-window-handles driver)))
-        content))))
+      (wait 5)
+      (handle-content ((dispatch-content-fn (get-url driver)) driver))
+      (close-window driver)
+      (switch-window driver (first (get-window-handles driver)))
+      (recur driver (+ index 1)))))
 
 
-
-
-(click driver [{:tag :div :class "result c-container " :index 4} {:tag :h3} {:tag :a}])
-
-(switch-window driver (last (get-window-handles driver)))
-
-(fill-human driver {:css ".s_ipt"} "qq")
-
+#_(def driver (search-driver))
 
 #_(souhu-search driver "纽仕达")
+#_(baidu-search driver "奔驰")
+
+#_(baidu-current-page 1)
+
 #_(go driver (str "http://search.sohu.com/?keyword=" "纽仕达"))
 
 #_(for [i (range 20)]
